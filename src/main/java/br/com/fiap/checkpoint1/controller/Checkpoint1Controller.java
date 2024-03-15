be0f2c365b5ce986c6ba1b6f05fb95ea72a1ea9a
TiagoAlcan <tiago.gomesalcan@gmail.com>
2024-03-15 01:33:44 -0300

    correcao de pacotes

--- src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
+++ src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
@@ -1,4 +1,4 @@
-package br.com.fiap.checkpoint.controller;
+package br.com.fiap.checkpoint1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Checkpoint1Controller {
 
-		@GetMapping("/ping")
-		public String ping() {
-			return "Pong";
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
 	}
 		
 }
